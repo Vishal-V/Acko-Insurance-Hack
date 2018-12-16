f46118e2c42ceb94cc1e652b6f6d2087b2806025
Vishal-V <vishal114186@gmail.com>
2018-12-16 09:14:45 +0530

    Final Subminal Submission!

--- Flask/simple.py
+++ Flask/simple.py
@@ -6,22 +6,22 @@ app = Flask(__name__, static_url_path="", static_folder="static")
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title='Acko Hack')
 
 @app.route('/fire')
 def firepre():
-    return render_template("fire.html")
+    return render_template("fire.html", title='Fire Prediction')
 
 @app.route('/land')
 def landpre():
-    return render_template("landslide.html")
+    return render_template("landslide.html", title='Landslide prediction')
 
 @app.route("/analyse")
 def analyse():
-	return render_template("Insurance.html")
+	return render_template("Insurance.html", title='Insurance')
 
 @app.route("/map")
 def map():
-	return render_template("map.html")
+	return render_template("map.html", title='Map')
 
 app.run(debug=True)
